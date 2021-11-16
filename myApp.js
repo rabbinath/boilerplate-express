0be2f5e0a8f97591d15b3ca916d420eccad67759
Rabindra Nath <rabbinath@gmail.com>
2021-11-16 19:03:04 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -5,7 +5,7 @@ absolutePath = __dirname + "/views/index.html";
 pubPath=__dirname + "/public";
 app.use("/public",express.static(pubPath));
 app.get('/', (req, res) => res.sendFile(absolutePath));
-
+app.get('/json ',(req, res) => res.json({"message": "Hello json"}))
 // Start the Express server
 //app.listen(3000, () => console.log('Server running on port 3000!'))
 
