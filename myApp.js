d2fcf8fa1204f45dd79780af04055cae83410a92
Rabindra Nath <rabbinath@gmail.com>
2021-11-16 12:27:58 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
-console.log("Hello World")
+console.log("Hello World");
+app.get('/', (req, res) => res.send('Hello Express!'))
+// Start the Express server
+//app.listen(3000, () => console.log('Server running on port 3000!'))
+
 
 
 
