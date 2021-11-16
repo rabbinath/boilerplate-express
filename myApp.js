76eff45104ee65aa69900596a4660e0c550a54fe
Rabindra Nath <rabbinath@gmail.com>
2021-11-16 18:23:40 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 //console.log("Hello World");
 absolutePath = __dirname + "/views/index.html";
-app.use("", express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 app.get('/', (req, res) => res.sendFile(absolutePath));
 
 // Start the Express server
