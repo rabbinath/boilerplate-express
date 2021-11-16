20f1d70e82b131bab4878eba8d7a9948475dbc1c
Rabindra Nath <rabbinath@gmail.com>
2021-11-16 19:53:52 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,14 +1,22 @@
 var express = require('express');
 var app = express();
+var dotenv=require("dotenv").config();
 //console.log("Hello World");
 absolutePath = __dirname + "/views/index.html";
 pubPath=__dirname + "/public";
 app.use("/public",express.static(pubPath));
 app.get("/", (req, res) => res.sendFile(absolutePath));
 app.get("/json",(req, res) => {
-    res.json({
-        message: "Hello json"
-    });
+
+    if (process.env.MESSAGE_STYLE==="upper")
+    {
+        res.json({ message: "HELLO JSON"});
+    }
+    else
+    {
+        res.json({ message: "Hello json"});
+    }
+
 });
 // Start the Express server
 //app.listen(3000, () => console.log('Server running on port 3000!'))
