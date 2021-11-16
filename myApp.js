6aeed63c9e61e1de4631ec0feb1a92d7a745fc33
Rabindra Nath <rabbinath@gmail.com>
2021-11-16 18:10:03 +0530

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var app = express();
 //console.log("Hello World");
-absolutePath = __dirname + relativePath/views/index.html
+absolutePath = __dirname + "/views/index.html";
 app.get('/', (req, res) => res.sendFile(absolutePath));
 
 // Start the Express server
-//app.listen(3000, () => console.log('Server running on port 3000!'))
+app.listen(3000, () => console.log('Server running on port 3000!'))
 
 
 
